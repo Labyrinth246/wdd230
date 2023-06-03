3b2fdff178c6b229fc795aaf9bb899c2b91cfbdc
Spencer Baker <spencer.baker77@gmail.com>
2023-06-03 14:36:21 -0600

    finished discover imagesection

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,36 +1,55 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
+document.addEventListener("DOMContentLoaded", function() {
+    if (typeof(Storage) !== "undefined") {
+    var lastVisit = localStorage.getItem("lastVisit");
+    var currentDate = new Date();
+
+    var timeDiff = currentDate - new Date(lastVisit);
+
+    //ms to days
+    var daysDiff = Math.round(timeDiff / (1000 * 60 * 60 * 24));
+
+    var resultElement = document.getElementById("visit");
+    resultElement.innerHTML = "Days since your last visit: " + daysDiff;
+
+    localStorage.setItem("lastVisit", currentDate);
+    } else {
+    console.log("Local storage is not supported");
+    }
+});
+
+
+
+
+  const allImages = document.querySelectorAll("img[data-src]");
+
+  const lazyLoad = (img) => {
+    img.setAttribute("src", img.getAttribute("data-src"));
+    img.onload = () => {
+      img.removeAttribute("data-src");
+      img.className = "in";
+    };
   };
-};
-
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-
-
-const imgOptions = {
+  
+  const options = {
     threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
-};
-
-
+    rootMargin: "0px 0px 50px 0px",
+  };
+  
   if ("IntersectionObserver" in window) {
-    const observer = new IntersectionObserver((items, observer) => {
+    const obsrvr = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
         if (item.isIntersecting) {
-          loadImages(item.target);
+          lazyLoad(item.target);
           observer.unobserve(item.target);
         }
       });
-    }, imgOptions);
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
+    }, options);
+    allImages.forEach((img) => {
+      obsrvr.observe(img);
     });
   } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
+    allImages.forEach((img) => {
+      lazyLoad(img);
     });
   }
+  
